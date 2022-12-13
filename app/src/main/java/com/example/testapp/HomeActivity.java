0b044f2e80de823de9ecf6e25437e228cfbeb478
panipaq <94040330+panipaq@users.noreply.github.com>
2022-12-13 09:42:14 +0100

    Menü zugefügt und Activities angelegt

--- app/src/main/java/com/example/testapp/HomeActivity.java
+++ app/src/main/java/com/example/testapp/HomeActivity.java
@@ -11,6 +11,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.example.testapp.databinding.ActivityHomeBinding;
+
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
@@ -21,11 +23,31 @@ public class HomeActivity extends AppCompatActivity {
     // Reference to the EntriesDataSource-Object
     private EntriesDataSource dataSource;
     Button btn_addEntry;
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        /*** Menubar ***/
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    startActivity(new Intent(this, HomeActivity.class));
+                    break;
+                case R.id.addEntry:
+                    startActivity(new Intent(this, EntriesActivity.class));
+                    break;
+                case R.id.statistics:
+                    startActivity(new Intent(this, StatisticsActivity.class));
+                    break;
+            }
+            return true;
+        });
 
 
         /****** Database *******/
@@ -74,7 +96,7 @@ public class HomeActivity extends AppCompatActivity {
         // Pass a predefined standard layout as layout for the entries
         ArrayAdapter<Entries> entriesArrayAdapter = new ArrayAdapter<>(
                 this,
-                android.R.layout.simple_list_item_multiple_choice,
+                android.R.layout.simple_list_item_1,
                 entriesList);
 
         // Put Entries onto Homescreen
