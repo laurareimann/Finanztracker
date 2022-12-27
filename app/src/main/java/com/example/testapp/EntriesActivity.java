0e79522694b5503338aaa3111ed81733ca93227b
Julia <julia.grohner@haw-hamburg.de>
2022-12-27 10:04:11 +0100

    Datum wird in der Ein/Ausgabe gezeigt

--- app/src/main/java/com/example/testapp/EntriesActivity.java
+++ app/src/main/java/com/example/testapp/EntriesActivity.java
@@ -8,7 +8,11 @@ package com.example.testapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -16,24 +20,32 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Calendar;
 
 public class EntriesActivity extends AppCompatActivity {
 
     // LOGTAG
     public static final String LOG_TAG = EntriesActivity.class.getSimpleName();
+    private static final String TAG = "EntriesActivity";
 
     private EntriesDataSource dataSource;
     Button btn_backHome;
     BottomNavigationView bNV_entries;
 
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entries);
+        mDisplayDate = (TextView) findViewById(R.id.editxt_entries_date);
 
         /*** Menubar ***/
         // Initialize and assign variable
@@ -72,6 +84,37 @@ public class EntriesActivity extends AppCompatActivity {
         dataSource.open();
 
         activateAddButton();
+
+        // Date
+        mDisplayDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        EntriesActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                //shows date in entry
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // January would be month 0 und December would be month 11 so:
+                month = month + 1;
+                Log.d(TAG,"ondateSet: mm/dd/yyy: " + month + "/" + day + "/" + year );
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+
+            }
+        };
     }
 
     @Override
@@ -108,6 +151,7 @@ public class EntriesActivity extends AppCompatActivity {
                 String amountString = editTextAmount.getText().toString();
                 String notice = editTextNotice.getText().toString();
                 String kategoryString = editTextKategory.getText().toString();
+
 
                 // Falls Eingabefelder leer, gib eine Errornachricht zurück
                 if (TextUtils.isEmpty(amountString)) {
